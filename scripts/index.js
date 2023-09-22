5c61ce5aae164d95fefd4b9ca93e40c316240847
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-09-22 12:11:10 +0200

    [DONE] used of methods

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
-'use strict'
+"use strict";
 
+const {
+  letters,
+  words,
+  upperString,
+  titleString,
+  backwardsLetters,
+  backwardsWords,
+  palindrome,
+} = require("./functions");
 
+const frase = prompt("Introduzca una frase");
+
+console.log(`Frase : "${frase}"`);
+console.log(`${letters(frase)} letras y ${words(frase)} palabras`);
+console.log(`${upperString(frase)}`);
+console.log(`${titleString(frase)}`);
+console.log(`${backwardsLetters(frase)}`);
+console.log(`${backwardsWords(frase)}`);
+console.log(`${palindrome(frase) ? "Sí" : "No"} es un palídromo`);
