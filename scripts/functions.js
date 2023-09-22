4f318d71de58b28ace38dee253ee9ff8d41baa05
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-09-22 11:57:57 +0200

    [DONE] deleteDiacritialText method

--- scripts/functions.js
+++ scripts/functions.js
@@ -28,7 +28,22 @@ function backwardsWords(cadena) {
   return String(cadena).split(" ").reverse().join(" ");
 }
 
-function palindrome(cadena) {}
+function palindrome(cadena) {
+  // https://stackoverflow.com/questions/6507056/replace-all-whitespace-characters
+  // También cabe la posibilidad de usar el bucle map, como hice en la funcion titleString
+  const textCleanned = deleteDiacritialText(
+    String(cadena).toLowerCase().replace(/\s+/g, "")
+  );
+  return textCleanned === backwardsLetters(textCleanned);
+}
+
+function deleteDiacritialText(cadena) {
+  // https://es.stackoverflow.com/questions/62031/eliminar-signos-diacr%C3%ADticos-en-javascript-eliminar-tildes-acentos-ortogr%C3%A1ficos
+  return cadena
+    .normalize("NFD")
+    .replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi, "$1$2")
+    .normalize();
+}
 
 module.exports = {
   letters,
