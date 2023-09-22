a00d77f892064373a6e5d06d5f0e3fc9827a226e
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-09-22 23:54:23 +0200

    [MOD] know if variable is empy or whites spaces

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,26 @@
 "use strict";
+const textEmptyError = "Error! No puedes no poner nada";
 
-const {
-  letters,
-  words,
-  upperString,
-  titleString,
-  backwardsLetters,
-  backwardsWords,
-  palindrome,
-} = require("./functions");
+startApp();
 
-const frase = prompt("Introduzca una frase");
+function startApp() {
+  const frase = prompt("Introduzca una frase");
 
-console.log(`Frase : "${frase}"`);
-console.log(`${letters(frase)} letras y ${words(frase)} palabras`);
-console.log(`${upperString(frase)}`);
-console.log(`${titleString(frase)}`);
-console.log(`${backwardsLetters(frase)}`);
-console.log(`${backwardsWords(frase)}`);
-console.log(`${palindrome(frase) ? "Sí" : "No"} es un palídromo`);
+  if (isEmptyOrSpaces(frase)) {
+    alert(textEmptyError);
+    return;
+  }
+
+  console.log(`Frase : "${frase}"`);
+  console.log(`${letters(frase)} letras y ${words(frase)} palabras`);
+  console.log(`${upperString(frase)}`);
+  console.log(`${titleString(frase)}`);
+  console.log(`${backwardsLetters(frase)}`);
+  console.log(`${backwardsWords(frase)}`);
+  console.log(`${palindrome(frase) ? "Sí" : "No"} es un palídromo`);
+}
+
+function isEmptyOrSpaces(str) {
+  // https://stackoverflow.com/questions/10232366/how-to-check-if-a-variable-is-null-or-empty-string-or-all-whitespace-in-javascri
+  return str === null || str.match(/^ *$/) !== null;
+}
